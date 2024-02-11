c832c260257eb5720e7d63329d4a28f46627c310
VithushaS <vithuselva1502@gmail.com>
2024-02-11 12:01:22 +0530

    bus booking full functions

---
+++ src/main/java/com/bus/booking/busBooking/repositories/BookingRepository.java
@@ -0,0 +1,12 @@
+package com.bus.booking.busBooking.repositories;
+
+import com.bus.booking.busBooking.entities.Booking;
+import com.bus.booking.busBooking.entities.Bus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.sql.Date;
+
+public interface BookingRepository extends JpaRepository<Booking,Long> {
+
+    int countByBusAndDate(Bus bus, Date date);
+}
